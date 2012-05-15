d6d00bc6536bbbcab04295b7b078efec64606670
Uttara Sakhadeo <uttarasakhadeo@gmail.com>
2012-05-15 15:44:05 +0000

    Updates Upto 15th May

--- src/edu/ucdenver/bios/studydesignsvc/application/StudyDesignApplication.java
+++ src/edu/ucdenver/bios/studydesignsvc/application/StudyDesignApplication.java
@@ -28,30 +28,52 @@ import org.restlet.Context;
 import org.restlet.Restlet;
 import org.restlet.routing.Router;
 
+import edu.ucdenver.bios.studydesignsvc.resource.BetaScaleRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.BetaScaleServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.BetweenParticipantRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.BetweenParticipantServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.ClusterNodeRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.ClusterNodeServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.ConfidenceIntervalRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.ConfidenceIntervalServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.CovarianceRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.CovarianceServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.CovarianceSetRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.CovarianceSetServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.DefaultResource;
+import edu.ucdenver.bios.studydesignsvc.resource.HypothesisRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.HypothesisServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.HypothesisSetRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.HypothesisSetServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.MatrixRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.MatrixServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.MatrixSetRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.MatrixSetServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.NominalPowerRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.NominalPowerServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.PowerCurveRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.PowerCurveServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.PowerMethodRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.PowerMethodServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.QuantileRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.QuantileServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.RelativeGroupSizeRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.RelativeGroupSizeServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.RepeatedMeasuresRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.RepeatedMeasuresServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.ResponsesRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.ResponsesServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.SampleSizeRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.SampleSizeServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.SaveAsServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.SigmaScaleRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.SigmaScaleServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.StatisticalTestRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.StatisticalTestServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.StudyDesignServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.StudyDesignUploadRetrieveServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.StudyRetrieveServerResource;
+import edu.ucdenver.bios.studydesignsvc.resource.TypeIErrorRetrieveServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.TypeIErrorServerResource;
 import edu.ucdenver.bios.studydesignsvc.resource.UploadServerResource;
 
@@ -94,64 +116,120 @@ public class StudyDesignApplication extends Application {
         router.attachDefault(DefaultResource.class);
         /* Study Design Resource */
         router.attach("/study", StudyDesignServerResource.class);
+        router.attach("/study/retrieve", StudyRetrieveServerResource.class);
         /* Study Design Retrieve Resource */// TODO: REMOVE THIS RESOURCE
-        //router.attach("/studyRetrieve", StudyDesignRetrieveServerResource.class);
+        // router.attach("/studyRetrieve",
+        // StudyDesignRetrieveServerResource.class);
         /* Study Design Upload Retrieve Resource */
-        router.attach("/studyUpload",
-                UploadServerResource.class);
+        router.attach("/studyUpload", UploadServerResource.class);
         router.attach("/studyUploadRetrieve",
                 StudyDesignUploadRetrieveServerResource.class);
         /* ConfidenceInterval Resource */
+
         router.attach("/confidenceIntervalDescription",
                 ConfidenceIntervalServerResource.class);
+        router.attach("/confidenceIntervalDescription/retrieve",
+                ConfidenceIntervalRetrieveServerResource.class);
         /* Power Curve Description */
         router.attach("/powerCurveDescription", PowerCurveServerResource.class);
+        router.attach("/powerCurveDescription/retrieve",
+                PowerCurveRetrieveServerResource.class);
 
         /* Beta Scale List Resource */
         router.attach("/betaScaleList", BetaScaleServerResource.class);
-        router.attach("/betaScaleList/{uuid}", BetaScaleServerResource.class);
+        router.attach("/betaScaleList/retrieve",
+                BetaScaleRetrieveServerResource.class);
+
         /* Alpha List Resource */
         router.attach("/alphaList", TypeIErrorServerResource.class);
+        router.attach("/alphaList/retrieve",
+                TypeIErrorRetrieveServerResource.class);
+
         /* Relative Group Size List Resource */
         router.attach("/relativeGroupSizeList",
                 RelativeGroupSizeServerResource.class);
+        router.attach("/relativeGroupSizeList/retrieve",
+                RelativeGroupSizeRetrieveServerResource.class);
+
         /* Sample Size List Resource */
         router.attach("/sampleSizeList", SampleSizeServerResource.class);
+        router.attach("/sampleSizeList/retrieve",
+                SampleSizeRetrieveServerResource.class);
+
         /* Sigma Scale List Resource */
         router.attach("/sigmaScaleList", SigmaScaleServerResource.class);
+        router.attach("/sigmaScaleList/retrieve",
+                SigmaScaleRetrieveServerResource.class);
+
         /* Statistical Test List Resource */
         router.attach("/testList", StatisticalTestServerResource.class);
+        router.attach("/testList/retrieve",
+                StatisticalTestRetrieveServerResource.class);
+
         /* Power Method List Resource */
         router.attach("/powerMethodList", PowerMethodServerResource.class);
+        router.attach("/powerMethodList/retrieve",
+                PowerMethodRetrieveServerResource.class);
+
         /* Quantile List Resource */
         router.attach("/quantileList", QuantileServerResource.class);
+        router.attach("/quantileList/retrieve",
+                QuantileRetrieveServerResource.class);
+
         /* Response List */
         router.attach("/responseList", ResponsesServerResource.class);
+        router.attach("/responseList/retrieve",
+                ResponsesRetrieveServerResource.class);
+
         /* Nominal Power List */
         router.attach("/nominalPowerList", NominalPowerServerResource.class);
+        router.attach("/nominalPowerList/retrieve",
+                NominalPowerRetrieveServerResource.class);
 
         /* Matrix Resource */
         router.attach("/matrix", MatrixServerResource.class);
+        router.attach("/matrix/retrieve", MatrixRetrieveServerResource.class);
         router.attach("/matrixSet", MatrixSetServerResource.class);
+        router.attach("/matrixSet/retrieve",
+                MatrixSetRetrieveServerResource.class);
+
         /* Between Subject Effect object Resource */
         router.attach("/betweenParticipantFactor",
                 BetweenParticipantServerResource.class);
+        router.attach("/betweenParticipantFactor/retrieve",
+                BetweenParticipantRetrieveServerResource.class);
+
         /* Clustering */
         router.attach("/clustering", ClusterNodeServerResource.class);
+        router.attach("/clustering/retrieve",
+                ClusterNodeRetrieveServerResource.class);
+
         /* Repeated Measures */
         router.attach("/repeatedMeasures", RepeatedMeasuresServerResource.class);
+        router.attach("/repeatedMeasures/retrieve",
+                RepeatedMeasuresRetrieveServerResource.class);
+
         /* Hypothesis object Resource */
         router.attach("/hypothesis", HypothesisServerResource.class);
+        router.attach("/hypothesis/retrieve",
+                HypothesisRetrieveServerResource.class);
         router.attach("/hypothesisSet", HypothesisSetServerResource.class);
+        router.attach("/hypothesisSet/retrieve",
+                HypothesisSetRetrieveServerResource.class);
+
         /* Covariance */
         router.attach("/covariance", CovarianceServerResource.class);
+        router.attach("/covariance/retrieve",
+                CovarianceRetrieveServerResource.class);
         router.attach("/covarianceSet", CovarianceSetServerResource.class);
-        
-        /* Study Design Save*/
-        router.attach("/saveas",SaveAsServerResource.class);
-        /*Study Design Upload*/
-        router.attach("/upload",UploadServerResource.class);
-        
+        router.attach("/covarianceSet/retrieve",
+                CovarianceSetRetrieveServerResource.class);
+
+        /* Study Design Save */
+        router.attach("/saveas", SaveAsServerResource.class);
+        /* Study Design Upload */
+        router.attach("/upload", UploadServerResource.class);
+
         return router;
 
     }
